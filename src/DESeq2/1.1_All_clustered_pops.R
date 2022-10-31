6d027a2e7c3ee44999ec5846d7ee1e86ba48f7e1
razofz <rasmus.olofzon@gmail.com>
2022-10-31 16:07:16 +0100

    snakemakeified DS1.1

--- src/DESeq2/1.1_All_clustered_pops.R
+++ src/DESeq2/1.1_All_clustered_pops.R
@@ -1,135 +1,141 @@
-source("env.R")
-raw_dir <- "data/raw/paper_specific/DEseq2/cluster_wise"
-# raw_dir <- "data/processed/notebooks/DEseq2/cluster_wise"
-out_dir <- "data/processed/DEseq2"
-if (dir.exists(out_dir) == FALSE) {
-  dir.create(out_dir, showWarnings = TRUE, recursive = TRUE)
-}
-suppressPackageStartupMessages(library("DESeq2"))
-suppressPackageStartupMessages(library("stringr"))
+invisible(lapply(list(
+  "stringr",
+  "DESeq2"
+), FUN = function(x) {
+  suppressPackageStartupMessages(library(x, character.only = T))
+}))
 
-files <- list.files(
-  path = raw_dir, pattern = "*_coldata_all_hpc.csv",
-  full.names = TRUE, recursive = FALSE
+set.seed(snakemake@config[["seed"]])
+
+clusters <- snakemake@config[["fl_clusters_to_use"]]
+cts_files <- snakemake@input[["cts_files"]]
+coldata_files <- snakemake@input[["coldata_files"]]
+normed_counts_files <- snakemake@output[["normed_counts"]]
+deseq_results_files <- snakemake@output[["deseq_results"]]
+
+print(clusters)
+
+named_cts_files <- unlist(
+  lapply(clusters,
+    FUN = function(cl) {
+      cts_files[str_detect(cts_files, str_c(cl, "_"))]
+    }
+  )
 )
-files <- files[-grep("DC.I", files)]
-files <- files[-grep("Ly.III", files)]
-print(files)
-if (dir.exists(paste0(out_dir, "/results/Normed_counts/")) == FALSE) {
-  dir.create(paste0(out_dir, "/results/Normed_counts/"),
-    showWarnings = TRUE,
-    recursive = TRUE
+stopifnot(length(named_cts_files) == length(clusters))
+named_coldata_files <- unlist(
+  lapply(clusters,
+    FUN = function(cl) {
+      coldata_files[str_detect(coldata_files, str_c(cl, "_"))]
+    }
   )
+)
+names(named_cts_files) <- clusters
+names(named_coldata_files) <- clusters
+
+named_normed_counts_files <- unlist(
+  lapply(clusters,
+    FUN = function(cl) {
+      normed_counts_files[str_detect(normed_counts_files, str_c(cl, "_"))]
+    }
+  )
+)
+named_deseq_results_files <- unlist(
+  lapply(clusters,
+    FUN = function(cl) {
+      deseq_results_files[str_detect(deseq_results_files, str_c(cl, "_"))]
+    }
+  )
+)
+names(named_normed_counts_files) <- clusters
+names(named_deseq_results_files) <- clusters
+
+if (snakemake@wildcards[["fl_core_version"]] == "FLcorePseudotech") {
+  deseq_design <- ~ pseudo_rep + sample
+} else if (snakemake@wildcards[["fl_core_version"]] == "FLcoreNoPseudotech") {
+  deseq_design <- ~sample
 }
 
-for (x in files) {
-  names <- str_split(str_split(x, "/", simplify = T)[6], "_coldata_all_hpc.csv",
-    simplify = T
-  )[1]
-  print(paste0("> names: ", names))
-
-  cts <- read.csv(paste0(raw_dir, "/", names, "_cts_all_hpc.csv"),
-    row.names =
-      1
+for (cluster in clusters) {
+  cts <- read.csv(named_cts_files[cluster],
+    row.names = 1
   )
-  coldata <- read.csv(x, row.names = 1)
+  coldata <- read.csv(named_coldata_files[cluster], row.names = 1)
   dds <- DESeqDataSetFromMatrix(
     countData = cts,
     colData = coldata,
-    design = ~pseudo_rep + sample
+    design = deseq_design
   )
   dds <- DESeq(dds, test = "LRT", reduced = ~1)
   res <- results(dds, contrast = c("sample", "FL", "yBM"))
-  write.table(res, paste0(out_dir, "/results/", names, "_FL_yBM.csv"),
+  write.table(res,
+    named_deseq_results_files[cluster],
     sep = ","
   )
   norm_counts <- counts(dds, normalized = T)
   write.table(norm_counts,
-    file = paste0(
-      out_dir, "/results/Normed_counts/",
-      names, "_DEseq_normed.txt"
-    ), append = F, sep = "\t",
+    file = named_normed_counts_files[cluster],
+    append = F, sep = "\t",
     row.names = TRUE, col.names = TRUE, quote = FALSE
   )
 }
 
-
 cts_combined <- data.frame()
 coldata_combined <- data.frame()
-names <- NULL
-
-for (x in 1:length(files)) {
-  if (x == 1) {
-  names <- str_split(str_split(files[x], "/", simplify = T)[6],
-    "_coldata_all_hpc.csv",
-    simplify = T
-  )[1]
-    print(names)
-  cts <- read.csv(paste0(raw_dir, "/", names, "_cts_all_hpc.csv"),
+for (cluster_idx in seq_len(length(clusters))) {
+  cts <- read.csv(named_cts_files[clusters[cluster_idx]],
     row.names = 1
   )
+  coldata <- read.csv(named_coldata_files[clusters[cluster_idx]],
+    row.names = 1
+  )
+  if (cluster_idx == 1) {
     cts_combined <- cts
-    coldata <- read.csv(files[x], row.names = 1)
     coldata_combined <- coldata
   } else {
-  names <- str_split(str_split(files[x], "/", simplify = T)[6],
-    "_coldata_all_hpc.csv",
-    simplify = T
-  )[1]
-    print(names)
-  cts <- read.csv(paste0(raw_dir, "/", names, "_cts_all_hpc.csv"),
-    row.names
-    = 1
-  )
     cts_combined <- cbind(cts_combined, cts)
-    coldata <- read.csv(files[x], row.names = 1)
     coldata_combined <- rbind(coldata_combined, coldata)
   }
 }
 
 cts_combined <- cts_combined[!grepl(
-  pattern = "Ly.III", x =
-    colnames(cts_combined)
+  pattern = "Ly.III", x = colnames(cts_combined)
 )]
 cts_combined <- cts_combined[!grepl(
-  pattern = "DC.I", x =
-    colnames(cts_combined)
+  pattern = "DC.I", x = colnames(cts_combined)
 )]
 cts_combined <- cts_combined[!grepl(
-  pattern = "_T_", x =
-    colnames(cts_combined)
+  pattern = "_T_", x = colnames(cts_combined)
 )]
 coldata_combined <- coldata_combined[!grepl(
-  pattern = "DC.I", x =
-    rownames(coldata_combined)
+  pattern = "DC.I", x = rownames(coldata_combined)
 ), ]
 coldata_combined <- coldata_combined[!grepl(
-  pattern = "Ly.III", x =
-    rownames(coldata_combined)
+  pattern = "Ly.III", x = rownames(coldata_combined)
 ), ]
 coldata_combined <- coldata_combined[!grepl(
-  pattern = "_T_", x =
-    rownames(coldata_combined)
+  pattern = "_T_", x = rownames(coldata_combined)
 ), ]
-cts_combined <- cts_combined[!grepl(
-  pattern = "yBM_", x =
-    colnames(cts_combined)
-)]
-coldata_combined <- coldata_combined[!grepl(
-  pattern = "yBM_", x =
-    rownames(coldata_combined)
-), ]
-cts_combined_clust <- cts_combined
-coldata_combined_clust <- coldata_combined
+# if (snakemake@wildcards[["fl_core_version"]] == "FLcoreNoPseudotech") {
+#   cts_combined <- cts_combined[!grepl(
+#     pattern = "yBM_", x = colnames(cts_combined)
+#   )]
+#   coldata_combined <- coldata_combined[!grepl(
+#     pattern = "yBM_", x = rownames(coldata_combined)
+#   ), ]
+# }
 
 dds <- DESeqDataSetFromMatrix(
   countData = cts_combined, colData = coldata_combined,
-  design = ~pseudo_rep + sample
+  design = deseq_design
+  # design = ~pseudo_rep + sample
 )
+
 dds <- DESeq(dds)
 
 norm_counts <- counts(dds, normalized = T)
 write.table(norm_counts,
- file = paste0(out_dir, "/results/Normed_counts/", "only_FL_DEseq_normed.txt"),
+  file = snakemake@output[["fl_only_normed_counts"]],
+  # file = paste0(out_dir, "/results/Normed_counts/", "only_FL_DEseq_normed.txt"),
   append = F, sep = "\t", row.names = TRUE, col.names = TRUE, quote = FALSE
 )
