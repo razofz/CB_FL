53c548330d13aa200bfc5bb97414d210f84732ae
razofz <rasmus.olofzon@gmail.com>
2022-11-14 17:11:51 +0100

    removed deprecated files from repo

--- src/DESeq2/env.R
+++
@@ -1,9 +0,0 @@
-library(readr)
-
-path <- Sys.getenv("PROJECT_PATH")
-if (nchar(path) == 0) {
-  path <- sub("\n", "", read_file("../../project_path"))
-}
-# print(getwd())
-setwd(path)
-print(paste0("> changed working dir to ", getwd()))
