c402584425dffe505c71fca4e9d08fd41d869e91
razofz <rasmus.olofzon@gmail.com>
2023-05-12 13:08:46 +0200

    nbs: step 3.2

--- notebooks/3.2_target_classification_FL_yBM.py
+++ notebooks/3.2_target_classification_FL_yBM.py
@@ -8,12 +8,12 @@
 #       format_version: '1.5'
 #       jupytext_version: 1.13.1
 #   kernelspec:
-#     display_name: Python 3 (ipykernel)
+#     display_name: notebooks_FL
 #     language: python
 #     name: python3
 # ---
 
-# +
+# + tags=[]
 # %load_ext autotime
 # %config InlineBackend.figure_format = 'retina'
 
@@ -38,20 +38,21 @@ def clean_axis(ax, ts=11, ga=0.4):
     return True
 
 
-# -
-
+# + tags=[]
 fl_utils.set_project_path()
 
+# + tags=[]
 raw_dir = "data/raw/paper_specific/from_seurat/"
 zarr_dir = "data/processed/notebooks/zarr_files"
 out_dir = "data/processed/notebooks"
 sup_tables_dir = out_dir + "/Supplemental_tables"
 figures_dir = out_dir + "/Figures"
 
+# + tags=[]
 fl_utils.create_dir(sup_tables_dir)
 fl_utils.create_dir(out_dir + "/mapping_predictions")
 
-# +
+# + tags=[]
 in_files = {
     "zarr_yBM": zarr_dir + "/RNA_yBM_hpc_merged",
     "zarr_FL": zarr_dir + "/RNA_FL_hpc_merged",
@@ -64,6 +65,7 @@ out_files = {
     "best_nn_FL": out_dir + "/mapping_predictions/RNA_FL_best_nn.json",
     "table_FL_on_yBM": sup_tables_dir + "/predictions_FL_hpc_on_yBM_hpc.csv",
     "table_yBM_on_FL": sup_tables_dir + "/predictions_yBM_hpc_on_FL_hpc.csv",
+    "classified_cells": out_dir + "/mapping_predictions/classified_cells.json",
     "plot_bar_ybm_hpc_on_FL_hpc": figures_dir
     + "/prediction_barplots_ybm_hpc_on_FL_hpc.svg",
     "plot_bar_FL_HPC_on_ybm_hpc": figures_dir
@@ -77,9 +79,14 @@ fl_utils.print_files(out_files, "out")
 
 # ---
 
+# + tags=[]
 ds_yBM = scarf.DataStore(in_files["zarr_yBM"], default_assay="RNA")
 ds_FL = scarf.DataStore(in_files["zarr_FL"], default_assay="RNA")
+# -
 
+Counter(ds_FL.cells.fetch("RNA_cluster_names"))
+
+# + tags=[]
 color_palette_BM = {}
 x = pd.crosstab(
     ds_yBM.cells.fetch("RNA_cluster_colors"),
@@ -88,6 +95,7 @@ x = pd.crosstab(
 for i in x:
     color_palette_BM[i] = x[i][x[i] > 0].index[0]
 
+# + tags=[]
 color_palette_FL = {}
 x = pd.crosstab(
     ds_FL.cells.fetch("RNA_cluster_colors"),
@@ -96,6 +104,7 @@ x = pd.crosstab(
 for i in x:
     color_palette_FL[i] = x[i][x[i] > 0].index[0]
 
+# + tags=[]
 classified_cells_BM = {}
 for sample in sorted(list(ds_yBM.z.RNA.projections.keys())):
     classified_cells_BM[sample] = ds_yBM.get_target_classes(
@@ -105,6 +114,7 @@ for sample in sorted(list(ds_yBM.z.RNA.projections.keys())):
     )
     print(sample, len(classified_cells_BM[sample]))
 
+# + tags=[]
 classified_cells_FL = {}
 for sample in sorted(list(ds_FL.z.RNA.projections.keys())):
     classified_cells_FL[sample] = ds_FL.get_target_classes(
@@ -114,7 +124,7 @@ for sample in sorted(list(ds_FL.z.RNA.projections.keys())):
     )
     print(sample, len(classified_cells_FL[sample]))
 
-# +
+# + tags=[]
 samples_pred_table_BM = {}
 targets_reodered = ["FL_CS16", "FL_HPC", "FL_W9", "CB_HPC", "yBM_HPC"]
 for n, sample in enumerate(targets_reodered):
@@ -154,14 +164,14 @@ plt.tight_layout()
 plt.savefig(out_files["plot_bar_FL_HPC_on_ybm_hpc"], dpi=300)
 plt.show()
 
-# +
+# + tags=[]
 samples_pred_table_FL = {}
 targets_reodered = ["FL_CS16", "FL_HPC", "FL_W9", "CB_HPC", "yBM_HPC"]
 for n, sample in enumerate(targets_reodered):
     df = classified_cells_FL[sample].value_counts(ascending=True).drop("NA")
     samples_pred_table_FL[sample] = 100 * df / df.sum()
 samples_pred_table_FL = pd.DataFrame(samples_pred_table_FL).fillna(0)
-samples_pred_table_FL.to_csv(out_files["table_yBM_on_FL"])
+# samples_pred_table_FL.to_csv(out_files["table_yBM_on_FL"])
 
 clust_order = [
     "HSC",
@@ -190,6 +200,20 @@ plt.savefig(out_files["plot_bar_ybm_hpc_on_FL_hpc"], dpi=300)
 # plt.savefig(f'./images/prediction_barplots_ybm_hpc_on_FL_hpc.svg', dpi=300)
 plt.show()
 # -
+
+classified_cells_FL
+
+cc_fl = {k: list(v) for k, v in classified_cells_FL.items()}
+
+import json
+with open(out_files["classified_cells"], "w") as f:
+    f.write(json.dumps(cc_fl))
+
+{k: Counter(v) for k, v in classified_cells_FL.items()}
+
+ds_FL
+
+Counter(ds_FL.cells.fetch("RNA_cluster_names"))
 
 samples_pred_table_FL
 
